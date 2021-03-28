662e83aec12f33960ea87d59b07d313caaa24dab
Graham Riches <Graham@attabotics.com>
2021-03-28 10:00:13 -0600

    Almost done chapter one exercises! One section left!

--- 1. Building Abstractions with Procedures/1.3 Formulating Abstractions With Higher-Order Procedures/higher_order_procedures.rkt
+++ 1. Building Abstractions with Procedures/1.3 Formulating Abstractions With Higher-Order Procedures/higher_order_procedures.rkt
@@ -123,3 +123,148 @@
   (filtered-accumulate + even? 0 identity a inc b))
 
 (sum-even 1 10)
+
+
+;-----------------------------------------------------------------------------------------------------
+; Exercise 1.34 -> some basic examples with lambdas and lets
+
+(define (new-pi-sum a b)
+  (sum (lambda (x) (/ 1.0 (* x (+ x 2))))
+       a
+       (lambda (x) (+ x 4))
+       b))
+
+(new-pi-sum 1 10)
+
+(define (square x)
+  (* x x))
+
+(define (f1 x y)
+  (let ((a (+ 1 (* x y)))
+        (b (- 1 y)))
+    (+ (* x (square a))
+       (* y b)
+       (* a b))))
+
+(define (f g)
+  (g 2))
+
+(f square)
+
+(f (lambda (z) (* z (+ z 1))))
+
+
+; if you expand this out, you get: g(f(2)) -> (f f) -> (f 2) -> (2 2) -> error
+
+
+;-----------------------------------------------------------------------------------------------------
+; Exercise 1.35 -> finding fixed points
+(define tolerance 0.00001)
+
+(define (fixed-point f first-guess)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2)) tolerance))
+  (define (try guess)
+    (newline)
+    (display guess)
+    (let ((next (f guess)))
+      (if (close-enough? guess next)
+          next
+          (try next))))
+  (try first-guess))
+
+(fixed-point cos 1.0)
+(cos 0.7390822985224024)
+
+(fixed-point (lambda (x) (+ 1 (/ 1 x))) 1.0)
+
+; phi^2 = phi + 1
+; or 1 + (1/x) = x -> x^2 = x + 1 -> they are the same function
+
+;-----------------------------------------------------------------------------------------------------
+; Exercise 1.36 - more fixed points
+; Note: fixed point function modified above
+
+(define (header x)
+  (newline)
+  (newline)
+  (display x)
+  (newline))
+
+(define (average x y)
+  (/ (+ x y) 2))
+
+(define (average-damp f)
+  (lambda (x) (average x (f x))))
+
+(header "exercise 1.36 - no damping")
+(fixed-point (lambda (x) (/ (log 1000) (log x))) 1.1)
+
+; notably faster with average damping added in :)
+
+;-----------------------------------------------------------------------------------------------------
+; Exercise 1.37 - continued fractions
+; bleh need to get my wrapped around recursion better again!
+
+(define (cont-frac n d k)
+  (define (iter result term)
+    (if ( = term 0)
+        result
+        (iter (/ (n term) (+ (d term) result)) (- term 1)))
+    )
+  (iter 0 k))
+
+(header "exercise 1.37 - iterative")
+(cont-frac (lambda (i) 1.0) (lambda (i) 1.0) 5)
+(cont-frac (lambda (i) 1.0) (lambda (i) 1.0) 6)
+(cont-frac (lambda (i) 1.0) (lambda (i) 1.0) 7)
+(cont-frac (lambda (i) 1.0) (lambda (i) 1.0) 8)
+
+(define (cont-frac-recursive n d k)
+  (cond ((= k 0) 0)
+        (else (/ (n k) (+ (d k) (cont-frac-recursive n d (- k 1)))))))
+
+(header "exercise 1.37 - recursive")
+(cont-frac-recursive (lambda (i) 1.0) (lambda (i) 1.0) 8)
+                              
+
+;-----------------------------------------------------------------------------------------------------
+; Exercise 1.38 - continued fraction approximation to e
+
+(header "exercise - 1.38")
+
+(define (euler-sequence i)
+  (cond ((= i 1) 1.0)
+        ((= i 2) 2.0)
+        ((= (remainder (- i 2) 3) 1) 1.0)
+        ((= (remainder (- i 2) 3) 2) 1.0)
+        ((= (remainder (- i 2) 3) 0) (+ 2.0 (* (/ (- i 2) 3.0) 2.0)))))
+
+
+(define (guess-euler n)
+  (+ 2 (cont-frac (lambda (i) 1.0) euler-sequence n)))
+
+(guess-euler 10)
+
+;-----------------------------------------------------------------------------------------------------
+; Exercise 1.39 - continued fraction to tan
+; Note: needed a bit of a hack to remap the cont-frac to use a subtraction, could be abstracted better
+
+(header "exercise - 1.39")
+
+(define (cont-frac-minus n d k)
+  (define (iter result term)
+    (if ( = term 0)
+        result
+        (iter (/ (n term) (- (d term) result)) (- term 1)))
+    )
+  (iter 0 k))
+
+(define (tan-cf x k)
+  (cont-frac-minus
+   (lambda (i) (cond ((= i 1) x)
+                     (else (square x))))
+   (lambda (i) (+ 1.0 (* 2.0 (- i 1))))
+   k))
+
+(tan-cf (/ 3.1415926 4) 15)  ; should be 1.0 ish :)
